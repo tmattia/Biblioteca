186abeb3fe7e783ebf8e57e0deebe03b77b08820
Tomas Mattia <tomas.mattia@gmail.com>
2012-01-16 00:56:08 -0200

    Added list books action

---
+++ src/com/twu/biblioteca/ListBooksAction.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+public class ListBooksAction implements Action {
+	
+	final static public String LIST_OF_BOOKS_MSG = "List of Books in the library:";
+	
+	private Printer printer;
+	private ArrayList<Book> booksList;
+
+	public ListBooksAction(Printer printer, ArrayList<Book> booksList) {
+		this.printer = printer;
+		this.booksList = booksList;
+	}
+
+	@Override
+	public void execute() {
+		printer.println(LIST_OF_BOOKS_MSG);
+		for (Book book : booksList) {
+			printer.println(book.toString());
+		}
+	}
+
+}
---
+++ test/com/twu/biblioteca/ListBooksActionTest.java
@@ -0,0 +1,36 @@
+package com.twu.biblioteca;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class ListBooksActionTest {
+	
+	@Test
+	public void shouldListBooks() {
+		final String FIRST_BOOK = "1. First Book";
+		final String SECOND_BOOK = "1. Second Book";
+		
+		Printer mockedPrinter = mock(Printer.class);
+		Book mockedBook1 = mock(Book.class);
+		when(mockedBook1.toString()).thenReturn(FIRST_BOOK);
+		Book mockedBook2 = mock(Book.class);
+		when(mockedBook2.toString()).thenReturn(SECOND_BOOK);
+		
+		ArrayList<Book> booksList = new ArrayList<Book>();
+		booksList.add(mockedBook1);
+		booksList.add(mockedBook2);
+		ListBooksAction listBooksAction = new ListBooksAction(mockedPrinter, booksList);
+		listBooksAction.execute();
+
+		verify(mockedPrinter).println(ListBooksAction.LIST_OF_BOOKS_MSG);
+		verify(mockedPrinter).println(FIRST_BOOK);
+		verify(mockedPrinter).println(SECOND_BOOK);
+	}
+
+}
