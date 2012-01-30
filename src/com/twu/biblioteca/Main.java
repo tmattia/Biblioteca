37ab85d4afed73f9655a7b5d058c9cd635c2d319
Tomas Mattia <tomas.mattia@gmail.com>
2012-01-30 13:24:47 -0200

    Refactored Main class

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -3,31 +3,32 @@ package com.twu.biblioteca;
 import java.util.ArrayList;
 
 public class Main {
+	
+	private Console console;
+	private Authenticator authenticator;
+	private BookList bookList;
+	private MovieList movieList;
+	
+	public Main() throws InvalidRatingScoreException {
+		console = new Console();
+		bookList = createBookList();
+		movieList = createMovieList();
+		authenticator = createAuthenticator();
+	}
+	
+	public void run() {
+		Application app = new Application(console, createMenu());
+		app.start();
+	}
 
 	public static void main(String[] args) throws InvalidRatingScoreException {
-		Console console = new Console();
-		UserList userList = new UserList();
-		User user = new User("111-1111", "password");
-		userList.add(user);
-		Login login = new Login(console, userList);
-		Authenticator authenticator = new Authenticator(null, login);
-
-		Book book1 = new Book(1, "First Book");
-		Book book2 = new Book(2, "Second Book");
-		BookList bookList = new BookList();
-		bookList.add(book1);
-		bookList.add(book2);
-		
+		Main main = new Main();
+		main.run();
+	}
+	
+	private Menu createMenu() {
 		ListRecordsAction listBooksAction = new ListRecordsAction(console, bookList);
 		MenuOption listBooksOption = new MenuOption(1, "List books", listBooksAction);
-		
-		Movie movie1 = new Movie("First Movie", "Director A", new Rating(10));
-		Movie movie2 = new Movie("Second Movie", "Director A", null);
-		Movie movie3 = new Movie("Third Movie", "Director B", new Rating(4));
-		MovieList movieList = new MovieList();
-		movieList.add(movie1);
-		movieList.add(movie2);
-		movieList.add(movie3);
 		
 		ListRecordsAction listMoviesAction = new ListRecordsAction(console, movieList);
 		MenuOption listMoviesOption = new MenuOption(2, "List movies", listMoviesAction);
@@ -43,10 +44,36 @@ public class Main {
 		options.add(listMoviesOption);
 		options.add(checkLibraryNumberOption);
 		options.add(reserveBookOption);
-		Menu menu = new Menu(options);
-
-		Application app = new Application(console, menu);
-		app.start();
+		
+		return new Menu(options);
+	}
+	
+	private BookList createBookList() {
+		Book book1 = new Book(1, "First Book");
+		Book book2 = new Book(2, "Second Book");
+		BookList bookList = new BookList();
+		bookList.add(book1);
+		bookList.add(book2);
+		return bookList;
+	}
+	
+	private MovieList createMovieList() throws InvalidRatingScoreException {
+		Movie movie1 = new Movie("First Movie", "Director A", new Rating(10));
+		Movie movie2 = new Movie("Second Movie", "Director A", null);
+		Movie movie3 = new Movie("Third Movie", "Director B", new Rating(4));
+		MovieList movieList = new MovieList();
+		movieList.add(movie1);
+		movieList.add(movie2);
+		movieList.add(movie3);
+		return movieList;
+	}
+	
+	private Authenticator createAuthenticator() {
+		UserList userList = new UserList();
+		User user = new User("111-1111", "password");
+		userList.add(user);
+		Login login = new Login(console, userList);
+		return new Authenticator(null, login);
 	}
 	
 }
